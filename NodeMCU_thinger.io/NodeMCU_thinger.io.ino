f494589c9160ba9ca8fd593c8d08cdcfa8ee6160
Muhammad Yasin <yasingammarkanari@gmail.com>
2024-01-14 19:53:02 +0700

    Update: Ganti platform IoT -> Thinger.io
    Berhasil baca data sensor dari arduino, kemudian mengirimnya ke Thinger.io

--- NodeMCU_thinger.io/NodeMCU_thinger.io.ino
+++ NodeMCU_thinger.io/NodeMCU_thinger.io.ino
@@ -1,12 +1,59 @@
 #include <ThingerESP8266.h>
 #include <ESP8266WiFi.h>
 
-void setup() {
-  
+// KONFIGURASI THINGER.IO
+#define USERNAME "theyayas"
+#define DEVICE_ID "NodeMCU_DHT22"
+#define DEVICE_CREDENTIAL "Tmi9VdBiGHtC8scg"
 
+// VARIABLE THINGER.IO
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
+// KONFIGURASI WIFI
+const char* ssid = "Perpustakaan Bergerak Free Wifi";
+const char* password = "vomi0406";
+
+// VARIABLE BACA DATA SENSOR
+float t, h;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // KONEKSI KE WIFI
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("..");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100); // sebagai indikator connecting ke wifi; setelah connect, LED akan mati/hidup
+  }
+
+  // HUBUNGKAN KE THINGER.IO
+  thing.add_wifi(ssid, password);
+
+  // MENGIRIM NILAI KE THINGER.IO
+  thing["DHT22"] >> [](pson & out) {
+    out["Temperature"]  = t;
+    out["Humidity"]     = h;
+  };
 }
 
 void loop() {
-  
+  thing.handle();
 
+  // MEMBACA DATA DARI ARDUINO UNO
+  if (Serial.available()>0){
+    String data = Serial.readStringUntil('\n'); //membaca baris
+    Serial.println(data);
+
+    int comma = data.indexOf(',');  // akan digunakan sebagai sebagai delimiter
+    if (comma != -1){
+      t = data.substring(0, comma).toFloat(); // mengambil data 0 sampai comma
+      h = data.substring(comma+1).toFloat();  // mengambil data setelah comma
+      Serial.print(t);
+      Serial.println(h);
+    }
+  }
 }
